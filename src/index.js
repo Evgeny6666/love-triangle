0841d9bcbfa46d216f31b83893a0ec862c9a2e6d
Evgenius <evgeny.depel@gmail.com>
2018-02-22 14:19:24 +0300

    add check for selfishness There is a cases, which does not cover test cases, where a lot of Spischonee love only yourself (for example [2,3,1,4,5,6] - "4", "5", "6" love yourself). My previous solution didn't solve this problem correctly, despite the fact that all test cases were completed successfully.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
     var count = 0;
     preferences.forEach(function(preference, i) {
-        var firstSpichonee = preferences[i] - 1,
-            secondSpichonee = preferences[firstSpichonee] - 1,
-            thirdSpichonee = preferences[secondSpichonee] - 1;
+        var firstSpichonee = preferences[i] - 1;
+        if (firstSpichonee !== i){ 
+            var secondSpichonee = preferences[firstSpichonee] - 1,
+                thirdSpichonee = preferences[secondSpichonee] - 1;
         if (i === thirdSpichonee){
             count++;
         }
-    })
-    return Math.floor(count / 3); // my implementation
+        }
+    });
+    return count / 3; // my implementation
 };
